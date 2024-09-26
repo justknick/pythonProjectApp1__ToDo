6a5ea10eb09b98e778064ead0bec50c275833c6d
justknick <126913469+justknick@users.noreply.github.com>
2024-09-25 21:13:44 -0400

    updated gui (add button now appends the 'todos' list

--- gui.py
+++ gui.py
@@ -3,11 +3,30 @@ import FreeSimpleGUI as sg
 
 
 label = sg.Text("Type in a To-Do Item: ")
-input_box = sg.InputText(tooltip="Enter todo")
-add_button = sg.Button("Add")
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
+add_button = sg.Button("Add", key="add")
 
-window = sg.Window('My Todo App', layout=[[label], "", [input_box, add_button]])
-window.read()
+button_quit = sg.Button("Close", key="quit")
+
+window = sg.Window('My Todo App',
+                   layout=[[label], "",
+                           [input_box, add_button],
+                           [button_quit]
+                           ],
+                   font=('Helvetica', 20)
+                   )
+while True:
+    event, values = window.read()
+    print(event, " ", values)
+    match event:
+        case "add":
+            todos = functions.get_todos()
+            new_todos = values['todo'] + "\n"
+            todos.append(new_todos)
+            functions.write_todos(todos)
+        case "close":
+            break
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
-
-
